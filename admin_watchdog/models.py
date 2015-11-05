15f95f0b3d703d966373baeabda61dd8dd711148
Nikolay Bryskin <nbryskin@gmail.com>
2015-11-05 19:46:16 +0300

    fixed varchar length bug

--- admin_watchdog/models.py
+++ admin_watchdog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
+class TruncatingCharField(models.CharField):
+    def get_prep_value(self, value):
+        value = super(TruncatingCharField,self).get_prep_value(value)
+        if value:
+            return value[:self.max_length]
+        return value
+
+
 class LogEntry(models.Model):
     class Meta:
         verbose_name = _("Log entry")
@@ -18,7 +26,7 @@ class LogEntry(models.Model):
         max_length=20,
         editable=False,
     )
-    shortmessage = models.CharField(
+    shortmessage = TruncatingCharField(
         _("Short message"),
         max_length=256,
         editable=False,
